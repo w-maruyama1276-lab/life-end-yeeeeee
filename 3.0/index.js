22791a53d07b9a04a8f6bee3587e62d1533d2c63
Maruyama Waka <w-maruyama1276@st.kyoto-art.ac.jp>
2026-05-14 16:03:02 +0900

    first commit

---
+++ 3.0/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const Database = require('better-sqlite3');
+const path = require('path');
+
+const app = express();
+const db = new Database(path.join(__dirname, 'votes.db'));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..')));
+
+const createTable = db.prepare(`
+  CREATE TABLE IF NOT EXISTS votes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    option TEXT NOT NULL
+  )
+`);
+createTable.run();
+
+const insertVote = db.prepare('INSERT INTO votes (option) VALUES (?)');
+const getVoteCounts = db.prepare('SELECT option, COUNT(*) AS count FROM votes GROUP BY option');
+const deleteAllVotes = db.prepare('DELETE FROM votes');
+
+app.post('/vote', (req, res) => {
+  const { option } = req.body;
+
+  if (!option || typeof option !== 'string') {
+    return res.status(400).json({ error: 'option is required' });
+  }
+
+  insertVote.run(option);
+  res.status(201).json({ success: true });
+});
+
+app.get('/votes', (req, res) => {
+  const votes = getVoteCounts.all();
+  res.json(votes);
+});
+
+app.delete('/votes', (req, res) => {
+  deleteAllVotes.run();
+  res.json({ success: true, message: 'All votes have been deleted' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
